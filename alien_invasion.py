a644f14a7d79a96adbe94fbce7df2ecd771a39fc
liqiang <liqiang@qq.com>
2024-08-06 22:43:18 +0800

    Done 12.6.4

---
+++ alien_invasion.py
@@ -0,0 +1,62 @@
+import sys
+import pygame
+
+from settings import Settings
+from ship import Ship
+# from practice_role import Role
+
+class AlienInvasion:
+    """管理游戏资源和行为的类"""
+
+    def __init__(self):
+        """初始化游戏并创建游戏资源"""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+        # self.role = Role(self)
+
+        #设置背景色。
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """开始游戏的主循环"""
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+ 
+            #让最近绘制的屏幕可见
+            pygame.display.flip()
+
+    def _check_events(self):
+        """响应按键和鼠标事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕"""
+        self.screen.fill(self.settings.bg_color) #fill()用于处理surface
+        self.ship.blitme()       
+        # self.role.blitme()
+
+if __name__ == '__main__':
+    #创建游戏实例并运行游戏
+    ai = AlienInvasion()
+    ai.run_game()
